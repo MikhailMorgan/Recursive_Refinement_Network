5bad0d4617e594ba985a1894eb2098788cb3f990
MikhailMorgan <mikhailmorgan@gmail.com>
2022-05-06 11:36:09 -0400

    Update RfR.py

--- RfR.py
+++ RfR.py
@@ -1,32 +1,94 @@
+######################################## Double Convolution
+class DoubleConv(nn.Module):
+    """(convolution => [BN] => ReLU) * 2"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+    
+######################################## Maxpooling followed by Double Convolution
+class Down(nn.Module):
+    """Downscaling with maxpool then double conv"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+
+######################################## Upsampling followed by Double Convolution
+class Up(nn.Module):
+    """Upscaling then double conv"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.up_conv = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0),
+        ) 
+        self.conv = DoubleConv(out_channels * 2, out_channels)
+
+
+    def forward(self, x1, x2):
+        x1 = self.up_conv(x1)
+        x = torch.cat([x1, x2], dim=1)
+        x = self.conv(x)
+        return x
+
+######################################## Output layer (1x1 Convolution followed by SoftMax activation)
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv_sigmoid = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        return self.conv_sigmoid(x)
+
 class RfR(nn.Module):
 
-    inter_in_channels = 64
-    last_in_channels = 32 + 3 # 32(context) + 3(flow)
+    #inter_in_channels = 64
+    #last_in_channels = 32 + 3 # 32(context) + 3(flow)
     
-    def __init__(self, name, n_channels, n_classes):
+    def __init__(self, name, in_channels, out_channels):
         super(RfR, self).__init__()
         self.name = name
-        self.n_channels = n_channels
-        self.n_classes = n_classes
-        self.inputL = DoubleConv(n_channels, 64)
-        self.down1 = Down(64, 64)
-        self.down2 = Down(64, 128)
-        self.down3 = Down(128, 128)
-        self.down4 = Down(128, 256)
-        self.down5 = Down(256, 256)
-        self.down6 = Down(256, 512)
-        self.down7 = Down(512, 512)
-        self.down8 = Down(512, 1024)
+        self.in_channels = in_channels
+        self.out_channels = out_channels
         
-        self.up1 = Up(1024, 512)
-        self.up2 = Up(512, 512)
-        self.up3 = Up(512, 256)
-        self.up4 = Up(256, 256)
-        self.up5 = Up(256, 128)
-        self.up6 = Up(128, 128)
-        self.up7 = Up(128, 64)
-        self.up8 = Up(64, 64)
-        self.outputL = OutConv(64, 3)
+        self.inputL = DoubleConv(self.in_channels, self.in_channels)
+        self.down1 = Down(self.in_channels, 2*self.in_channels)
+        self.down2 = Down(2*self.in_channels, 2*self.in_channels)
+        self.down3 = Down(2*self.in_channels, 4*self.in_channels)
+        self.down4 = Down(4*self.in_channels, 4*self.in_channels)
+        self.down5 = Down(4*self.in_channels, 8*self.in_channels)
+        self.down6 = Down(8*self.in_channels, 8*self.in_channels)
+        
+        self.up1 = Up(8*self.in_channels, 8*self.in_channels)
+        self.up2 = Up(8*self.in_channels, 4*self.in_channels)
+        self.up3 = Up(4*self.in_channels, 4*self.in_channels)
+        self.up4 = Up(4*self.in_channels, 2*self.in_channels)
+        self.up5 = Up(2*self.in_channels, 2*self.in_channels)
+        self.up6 = Up(2*self.in_channels, self.out_channels)
+        self.outputL = OutConv(self.in_channels, )
         
     def forward(self, x):
         x1 = self.inputL(x)
@@ -36,19 +98,23 @@ class RfR(nn.Module):
         x4 = self.down3(x3)
         x5 = self.down4(x4)
         x6 = self.down5(x5)
-        x7 = self.down6(x6)
-        x8 = self.down7(x7)
-        b = self.down8(x8)
+        b = self.down6(x6)
         
-        x = self.up1(b, x8)
-        x = self.up2(x, x7)
-        x = self.up3(x, x6)
-        x = self.up4(x, x5)
-        x = self.up5(x, x4)
-        x = self.up6(x, x3)
-        x = self.up7(x, x2)
-        x = self.up8(x, x1)
+        x = self.up1(b, x6)
+        x = self.up2(x, x5)
+        x = self.up3(x, x4)
+        x = self.up4(x, x3)
+        x = self.up5(x, x2)
+        x = self.up6(x, x1)
         
         x = self.outputL(x)
         
         return x
+        
+inp_channel = 3
+opt_channel = 2
+DVF_est = RfR('RunfastReg', inp_channel, opt_channel)
+if cuda == True:
+    my_UNet.cuda()
+else:
+    my_UNet
